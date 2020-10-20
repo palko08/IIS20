ba67d264c4702e084ae5361ed666e7f396a8d136
dnosko <dasa.noskova@gmail.com>
2020-10-20 22:24:31 +0200

    vyhladavanie

--- festivals.php
+++ festivals.php
@@ -4,15 +4,16 @@ require "common.php";
 make_header();
 ?>
 
-<body>
+<body id="festivals">
      <div class="bg-1">
 		<div class="container">
-			<h3 class="text-center" style="margin-top:150px; color:white">FESTIVALY</h3>
+			<h3 class="text-center" style="margin-top:150px; color:white; position:relative">FESTIVALY</h3>
+			<input type="text" class="form-control form-rounded" placeholder="Nájsť festival"> 
 			<div class="row text-center" style="background-color:rgb(31, 29, 30)">
 				<div class="col-sm-4">
 					<div class="thumbnail">
 					<img src="img/lir3.jpg" alt="Paris">
-					<p><strong>Paris</strong></p>
+					<p style="margin-top:5px"><strong>Paris</strong></p>
 					<p>1-3 August 2020</p>
 					<button class="btn">Buy Tickets</button>
 					</div>
@@ -20,7 +21,7 @@ make_header();
 				<div class="col-sm-4">
 					<div class="thumbnail">
 					<img src="img/lir2.jpg" alt="New York">
-					<p><strong>New York</strong></p>
+					<p style="margin-top:5px"><strong>New York</strong></p>
 					<p>Sat. 28 November 2015</p>
 					<button class="btn">Buy Tickets</button>
 					</div>
@@ -28,7 +29,7 @@ make_header();
 				<div class="col-sm-4">
 					<div class="thumbnail">
 					<img src="img/lir.jpg" alt="San Francisco">
-					<p><strong>San Francisco</strong></p>
+					<p style="margin-top:5px"><strong>San Francisco</strong></p>
 					<p>Sun. 29 November 2015</p>
 					<button class="btn">Buy Tickets</button>
 					</div>

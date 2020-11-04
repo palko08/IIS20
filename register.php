acac0ddf0a10bf8ee143c2c1c2634879859204df
dnosko <dasa.noskova@gmail.com>
2020-11-04 14:15:54 +0100

    register

--- register.php
+++ register.php
@@ -2,25 +2,44 @@
 	require "common.php";
 	make_header();
 ?>
+    <link rel="stylesheet" href="css/register.css">
 <br><br>
-<h1>Vytvorit novy ucet</h1>
-
-<form action="account_insert.php" method="post">
-	<label for="email">E-mail</label>
-    <input type="text" name="email" id="email"><br>
-
-    <label for="meno">Meno</label>
-    <input type="text" name="meno" id="meno"><br>
-
-    <label for="login">Login</label>
-    <input type="text" name="login" id="login"><br>
-    
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password"><br>
-    
-    <input type="submit" value="Register">
-</form>
-
+<div class="register">
+<div class="col-lg-4 col-md-4 col-md-offset-4" id="login">
+    <h1>Vytvoriť nový účet</h1>
+    <section id="inner-wrapper" class="login">
+        <article>
+    <form action="account_insert.php" method="post">
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user"> </i></span>
+                <input type="email" class="form-control" placeholder="Email" name="email" id="email"><br>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-envelope"> </i></span>
+                <input type="text" name="meno" id="meno"  class="form-control" placeholder="Meno"><br>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-envelope"> </i></span>
+                <input type="text" name="login" id="login" class="form-control" placeholder="Login"><br>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-key"> </i></span>
+                <input type="password" name="password" id="password" class="form-control" placeholder="Password"><br>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-success btn-block" value="Register">Registrovať</button>
+        <br>
+    </form>
+        </article>
+    </section></div>
+</div>
 <?php
 make_footer();
 ?>

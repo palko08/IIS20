8d25a3392d85639ed7b1b982a1cef6bd854a072e
dnosko <dasa.noskova@gmail.com>
2020-11-16 19:52:16 +0100

    add popis

--- festival_insert.php
+++ festival_insert.php
@@ -9,13 +9,19 @@ $adresa = $_POST['adresa'];
 $datum_od = $_POST['od'];
 $datum_do = $_POST['do'];
 $cena = $_POST['cena'];
-
+$popis = '';
 
 $festival = new Festival();
 $error = $festival->createNewFestival($pdo,$nazov,$kapacita, $datum_od, $datum_do,$cena,$adresa);
 if ($error == -1)
 {
     throw new Exception("Ucet sa nepodarilo vytvorit");
+}
+
+if (!(empty($_POST['popis']))) {
+    $popis = $_POST['popis'];
+    $id = $festival->getID();
+    $error = $festival->setPopis($pdo, $id);
 }
 
 header("Location: /admin.php");

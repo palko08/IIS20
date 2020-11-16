908cf7e20ad091bcd9b39ae0b2556d9f6edb2a43
palko08 <48720188+palko08@users.noreply.github.com>
2020-11-16 12:11:46 +0100

    Funkcna classa Festival
    Pridane vytvaranie noveho festivalu, vymazanie daneho festivalu

--- festivalClass.php
+++ festivalClass.php
@@ -4,15 +4,23 @@ class Festival{
 
 	private $festivalID;
 
+    function __construct(){
+
+    }
+
+    function __destruct(){
+
+    }
+
 	/**
- 	 *  @brief Funkcia pre inicializaciu instancie classy Festival
+ 	 *  @brief Funkcia pre priradenie instancie classy Festival k danemu rowu v databaze
  	 *
  	 *  @param pdo Nadviazane PDO spojenie s databazou
  	 *	@param id ID daneho rowu v databaze
  	 *
- 	 *	@return 0 ak sa ID najde, 1 ak sa ID nenajde
+ 	 *	@return 0 ak sa ID najde, -1 ak sa ID nenajde
  	 */
-    function __construct($pdo, $id){
+    function initExistingFestival($pdo, $id){
     	$idSelect = $pdo->prepare("SELECT festival_ID FROM Festival WHERE festival_ID = ?");
     	$idSelect->execute([$id]);
 
@@ -20,8 +28,58 @@ class Festival{
     		$this->festivalID = $idSelect->fetchColumn();
     		return 0;
     	}else{
-    		return 1;
+    		return -1;
     	}
+    }
+
+    /**
+     *  @brief Funkcia pre vytvorenie noveho rowu v databaze v tabulke Festival a priradenie daneho festivalu do instancie classy festival
+     *
+     *  @param pdo Nadviazane PDO spojenie s databazou
+     *  @param nazov, kapacita, datum_Od, datum_Do, adresa Parametre reprezentujuce data vkladane do tabulky
+     *
+     *  @return ID vlozeneho rowu, alebo ID uz existujuceho rowu, -1 ak sa nepodarilo vlozit dany row
+     */
+    function createNewFestival($pdo, $nazov, $kapacita, $datum_Od, $datum_Do, $adresa){
+        $testSelect = $pdo->prepare("SELECT festival_ID FROM Festival WHERE nazov = ? AND kapacita = ? AND datum_Od = ? AND datum_Do = ? AND adresa = ?");
+        $testSelect->execute([$nazov, $kapacita, $datum_Od, $datum_Do, $adresa]);
+
+        if($testSelect->rowCount() == 1){
+            return $testSelect->fetchColumn();
+        }else{
+            $insert = $pdo->prepare("INSERT INTO Festival(nazov, kapacita, datum_Od, datum_Do, adresa) VALUES(?, ?, ?, ?, ?)");
+            $insert->execute([$nazov, $kapacita, $datum_Od, $datum_Do, $adresa]);
+
+            $select = $pdo->prepare("SELECT festival_ID FROM Festival WHERE nazov = ? AND kapacita = ? AND datum_Od = ? AND datum_Do = ? AND adresa = ?");
+            $select->execute([$nazov, $kapacita, $datum_Od, $datum_Do, $adresa]);
+
+            if($select->rowCount() == 1){
+                $this->festivalID = $select->fetchColumn();
+                return $this->festivalID;
+            }else{
+                return -2;
+            }
+        }
+    }
+
+    /**
+     *  @brief Funkcia pre vymazanie rowu, ktoreho reprezentaciou je dana instancia classy Festival
+     *
+     *  @param pdo Nadviazane PDO spojenie s databazou
+     *
+     *  @return 0 ak sa delete podaril, -1 ak nie
+     */
+    function deleteFestival($pdo){
+        $delete = $pdo->prepare("DELETE FROM Festival WHERE festival_ID = ?");
+        $delete->execute([$this->festivalID]);
+
+        $select = $pdo->prepare("SELECT festival_ID FROM Festival WHERE festival_ID = ?");
+        $select->execute([$this->festivalID]);
+        if($select->rowCount() == 0){
+            return 0;
+        }else{
+            return -1;
+        }
     }
 
     /**

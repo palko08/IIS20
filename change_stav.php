f32923539a073ee4d1de69b1adfa657ff44b971d
dnosko <dasa.noskova@gmail.com>
2020-11-17 17:32:58 +0100

    files refactor

---
+++ change_stav.php
@@ -0,0 +1,18 @@
+<?php
+require_once "classVstupenka.php";
+require_once "connect_db.php";
+
+$pdo = connect_db();
+
+$stav = $_GET['stav'];
+$id = $_GET['id'];
+
+if (isset($_GET['id'])) {
+    $ticket = new Vstupenka();
+    $ticket->initExistingVstupenka($pdo,$id);
+    $ticket->setStav($pdo,$stav);
+}
+
+header("Location: /admin.php");
+die;
+?>
--- view/show_tickets.php
+++ view/show_tickets.php
@@ -1,9 +1,7 @@
 <?php
-require "classVstupenka.php";
-require_once ("classFestival.php");
-require_once "classNeregistrovany.php";
-require_once "classRegistrovany.php";
+require_once "classVstupenka.php";
 require_once "connect_db.php";
+require_once "tickets_controll.php";
 $pdo = connect_db();
 $idSelect = $pdo->prepare("SELECT vstupenka_ID FROM Vstupenka");
 $idSelect->execute();
@@ -49,12 +47,12 @@ foreach($results as $row) {
             <a class="no_color_change_link" id="stav"><?php echo $ticket->getStav($pdo); ?></a>
         </td>
         <td>
-            <a href="../tickets_functions.php?func=stav&stav=potvrdena&id=<?php echo $ticket->getID(); ?>"><button type="button" id="align-right" class="btn btn-info"> Potvrdiť </button></a>
-            <a href="../tickets_functions.php?func=stav&stav=stornovana&id=<?php echo $ticket->getID(); ?>"><button type="button" id="align-right" class="btn btn-danger"> Stornovať </button></a>
+            <a href="../change_stav.php?stav=potvrdena&id=<?php echo $ticket->getID(); ?>"><button type="button" id="align-right" class="btn btn-info"> Potvrdiť </button></a>
+            <a href="../change_stav.php?stav=stornovana&id=<?php echo $ticket->getID(); ?>"><button type="button" id="align-right" class="btn btn-danger"> Stornovať </button></a>
         </td>
         <td>
             <a class="no_color_change_link" id="cena"><?php echo get_cena($ticket,$pdo); ?></a></td>
-         <td>  <a href="../tickets_functions.php?func=stav&stav=vydana&id=<?php echo $ticket->getID(); ?>"><button type="button" id="align-right" class="btn btn-primary"> Vydať </button></>
+         <td>  <a href="../change_stav.php?stav=vydana&id=<?php echo $ticket->getID(); ?>"><button type="button" id="align-right" class="btn btn-primary"> Vydať </button></>
         </td>
     </tr>
         <?php
@@ -64,35 +62,5 @@ foreach($results as $row) {
     </tbody>
 </table>
 
-<?php
-function get_cena($ticket,$pdo){
-    $festival_id = $ticket->getFestival_ID($pdo);
-    $festival = new Festival();
-    if ($festival->initExistingFestival($pdo, $festival_id) == -1) {
-        echo "festival nenadjeny";
-    }
-    return $festival->getCena($pdo);
-}
 
-function get_email($ticket,$pdo){
-    $customer = null;
-
-    if ($ticket->getNeregistrovany_ID($pdo) != NULL) {
-        $id_customer = $ticket->getNeregistrovany_ID($pdo);
-        $customer = new Neregistrovany();
-        if ($customer->initExistingNeregistrovany($pdo, $id_customer) == -1){
-            echo "nenasli sme v databazke dany row<br>";
-        }
-    }
-    elseif ($ticket->getRegistrovany_ID($pdo) != NULL) {
-        $id_customer = $ticket->getRegistrovany_ID($pdo);
-        $customer = new Registrovany();
-        if ($customer->initExistingRegistrovany($pdo, $id_customer) == -1){
-            echo "nenasli sme v databazke dany row<br>";
-        }
-    }
-
-    return $customer->getEmail($pdo);
-}
-?>
 

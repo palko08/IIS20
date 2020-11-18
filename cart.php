f81fdd75ddff84ef46120bfeb088a605be403e95
Patrik Suba <xsubap00@stud.fit.vutbr.cz>
2020-11-18 15:24:33 +0100

    vytvoril som zobrazovanie v kosiku

--- cart.php
+++ cart.php
@@ -1,5 +1,37 @@
 <?php
 require "common.php";
+require "classVstupenka.php";
+require "services.php";
+require "tickets_controll.php";
+require_once "connect_db.php";
+
+$vstupenka = new Vstupenka();
+$serv = new AccountService();
+$pdo = connect_db();
+$person = $serv->getAccount($_SESSION['user']);
+$vstupenkaArray = $vstupenka->getAllVstupenka($pdo,$person['registrovany_ID']);
+
+function make_product($pdo, $vstupenka){
+    echo '<tr>
+                        <td class="col-sm-8 col-md-6">
+                        <div class="media">
+                            <div class="media-body">
+                                <h4 class="media-heading"><a href="festival_page.php?id='.$vstupenka->getFestival_ID($pdo).'">Vstupenka</a></h4>
+                            </div>
+                        </div></td>
+                        <td class="col-sm-1 col-md-1" style="text-align: center">
+                            <div class="number-input md-number-input">
+                                <input class="quantity" min="0" name="quantity" value="1" type="number">
+                            </div>
+                        </td>
+                        <td class="col-sm-1 col-md-1 text-center"><strong>'.get_cena($vstupenka,$pdo).'</strong></td>
+                        <td class="col-sm-1 col-md-1 text-center"><strong>'.get_cena($vstupenka,$pdo).'</strong></td>
+                        <td class="col-sm-1 col-md-1">
+                        <button type="button" class="btn btn-danger">
+                            <span class="glyphicon glyphicon-remove"></span> Odstrániť
+                        </button></td>
+                    </tr>';
+}
 
 make_header();
 ?>
@@ -21,50 +53,20 @@ make_header();
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td class="col-sm-8 col-md-6">
-                        <div class="media">
-                            <div class="media-body">
-                                <h4 class="media-heading"><a href="#">Product name</a></h4>
-                            </div>
-                        </div></td>
-                        <td class="col-sm-1 col-md-1" style="text-align: center">
-                            <div class="number-input md-number-input">
-                                <input class="quantity" min="0" name="quantity" value="1" type="number">
-                            </div>
-                        </td>
-                        <td class="col-sm-1 col-md-1 text-center"><strong>$4.87</strong></td>
-                        <td class="col-sm-1 col-md-1 text-center"><strong>$14.61</strong></td>
-                        <td class="col-sm-1 col-md-1">
-                        <button type="button" class="btn btn-danger">
-                            <span class="glyphicon glyphicon-remove"></span> Odstrániť
-                        </button></td>
-                    </tr>
-                    <tr>
-                        <td class="col-md-6">
-                        <div class="media">
-                            <div class="media-body">
-                                <h4 class="media-heading"><a href="#">Product name</a></h4>
-                            </div>
-                        </div></td>
-                        <td class="col-md-1" style="text-align: center">
-                            <div class="number-input md-number-input">
-                                <input class="quantity" min="0" name="quantity" value="1" type="number">
-                            </div>
-                        </td>
-                        <td class="col-md-1 text-center"><strong>$4.99</strong></td>
-                        <td class="col-md-1 text-center"><strong>$9.98</strong></td>
-                        <td class="col-md-1">
-                        <button type="button" class="btn btn-danger">
-                            <span class="glyphicon glyphicon-remove"></span> Odstrániť
-                        </button></td>
-                    </tr>
+                    <?php
+                    if ($vstupenkaArray[0] != NULL) {
+                        foreach ($vstupenkaArray as $vstup) {
+                            make_product($pdo, $vstup);
+                            $pay = $pay + get_cena($vstup,$pdo);
+                        }
+                    }
+                    ?>
                     <tr>
                         <td>   </td>
                         <td>   </td>
                         <td>   </td>
                         <td><h3>Spolu</h3></td>
-                        <td class="text-right"><h3><strong>$31.53</strong></h3></td>
+                        <td class="text-right"><h3><strong><?php echo $pay;?></strong></h3></td>
                     </tr>
                     <tr>
                         <td>   </td>
